da23f9fa66be14db0dbd19ac5ded4aa83937b10c
José González D'Amico <jose.gonzalez.d@gmail.com>
2013-06-27 08:28:37 -0300

    Alternative landscape layouts postponed / Appwidget-provider sizes in dimens

--- gen/com/josegd/monthcalwidget/R.java
+++ gen/com/josegd/monthcalwidget/R.java
@@ -23,16 +23,26 @@ public final class R {
         public static final int white=0x7f050000;
     }
     public static final class dimen {
-        public static final int dates_3x2=0x7f060006;
-        public static final int dates_4x3=0x7f060009;
-        public static final int days_week_3x2=0x7f060005;
-        public static final int days_week_4x3=0x7f060008;
-        public static final int month_year_3x2=0x7f060004;
-        public static final int month_year_4x3=0x7f060007;
-        public static final int widget_height_3x2=0x7f060001;
-        public static final int widget_height_4x3=0x7f060003;
-        public static final int widget_width_3x2=0x7f060000;
-        public static final int widget_width_4x3=0x7f060002;
+        public static final int dates_3x2=0x7f06000a;
+        public static final int dates_4x3=0x7f06000d;
+        public static final int days_week_3x2=0x7f060009;
+        public static final int days_week_4x3=0x7f06000c;
+        /**  Text sizes 
+         */
+        public static final int month_year_3x2=0x7f060008;
+        public static final int month_year_4x3=0x7f06000b;
+        public static final int widget_height_3x2=0x7f060005;
+        public static final int widget_height_4x3=0x7f060007;
+        public static final int widget_minheight_3x2=0x7f060001;
+        public static final int widget_minheight_4x3=0x7f060003;
+        /**  Min widget sizes in appwidget-provider 
+         */
+        public static final int widget_minwidth_3x2=0x7f060000;
+        public static final int widget_minwidth_4x3=0x7f060002;
+        /**  Widget sizes 
+         */
+        public static final int widget_width_3x2=0x7f060004;
+        public static final int widget_width_4x3=0x7f060006;
     }
     public static final class drawable {
         public static final int mcw_3x2_preview=0x7f020000;
