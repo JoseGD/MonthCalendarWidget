13f33ca3777d364d51da4fe082af5679c2bb8355
José González D'Amico <jose.gonzalez.d@gmail.com>
2013-09-17 18:11:52 -0300

    New instructions screen / Resources for all pixel densities

--- gen/com/josegd/monthcalwidget/R.java
+++ gen/com/josegd/monthcalwidget/R.java
@@ -16,7 +16,8 @@ public final class R {
     }
     public static final class color {
         public static final int background=0x7f050003;
-        public static final int instructions_background=0x7f050005;
+        public static final int instr_button=0x7f050006;
+        public static final int instrsteps_background=0x7f050005;
         public static final int lightgray=0x7f050001;
         public static final int settings_background=0x7f050004;
         public static final int today=0x7f050002;
@@ -27,8 +28,15 @@ public final class R {
         public static final int dates_4x3=0x7f060011;
         public static final int days_week_3x2=0x7f06000d;
         public static final int days_week_4x3=0x7f060010;
-        /**  Text sizes 
- Text sizes 
+        public static final int instr_btnwidth=0x7f060014;
+        public static final int instr_steppadding=0x7f060016;
+        public static final int instr_svsize=0x7f060015;
+        /**  Instructions sizes 
+ Instructions sizes 
+         */
+        public static final int instr_textsize_1=0x7f060012;
+        public static final int instr_textsize_2=0x7f060013;
+        /**  Widget text sizes 
          */
         public static final int month_year_3x2=0x7f06000c;
         public static final int month_year_4x3=0x7f06000f;
@@ -44,7 +52,6 @@ public final class R {
         public static final int widget_paddingB_3x2=0x7f060009;
         public static final int widget_paddingB_4x3=0x7f06000b;
         /**  Widget padding 
- Widget padding 
          */
         public static final int widget_paddingLR_3x2=0x7f060008;
         public static final int widget_paddingLR_4x3=0x7f06000a;
@@ -55,16 +62,17 @@ public final class R {
         public static final int widget_width_4x3=0x7f060006;
     }
     public static final class drawable {
-        public static final int mcw_3x2_preview=0x7f020000;
-        public static final int mcw_4x3_preview=0x7f020001;
-        public static final int monthcal=0x7f020002;
-        public static final int next_normal=0x7f020003;
-        public static final int next_pressed=0x7f020004;
-        public static final int previous_normal=0x7f020005;
-        public static final int previous_pressed=0x7f020006;
-        public static final int selector_next=0x7f020007;
-        public static final int selector_prev=0x7f020008;
-        public static final int widget_bg=0x7f020009;
+        public static final int ic_launcher=0x7f020000;
+        public static final int mcw_3x2_preview=0x7f020001;
+        public static final int mcw_4x3_preview=0x7f020002;
+        public static final int mcw_instr_3x2=0x7f020003;
+        public static final int next_normal=0x7f020004;
+        public static final int next_pressed=0x7f020005;
+        public static final int previous_normal=0x7f020006;
+        public static final int previous_pressed=0x7f020007;
+        public static final int selector_next=0x7f020008;
+        public static final int selector_prev=0x7f020009;
+        public static final int widget_bg=0x7f02000a;
     }
     public static final class id {
         public static final int date11=0x7f0a000e;
@@ -116,16 +124,24 @@ public final class R {
         public static final int day5=0x7f0a000a;
         public static final int day6=0x7f0a000b;
         public static final int day7=0x7f0a000c;
-        public static final int instructions_intro=0x7f0a003f;
-        public static final int instructions_text=0x7f0a0040;
-        public static final int instructions_title=0x7f0a003e;
-        public static final int main_instr_ll=0x7f0a003d;
+        public static final int instr_howtouse_sv=0x7f0a0042;
+        public static final int instr_howtouse_tl=0x7f0a0043;
+        public static final int instr_intro=0x7f0a003e;
+        public static final int instr_morehelp=0x7f0a0049;
+        public static final int instr_nav=0x7f0a0048;
+        public static final int instr_sampleHS_descr=0x7f0a0040;
+        public static final int instr_towork1=0x7f0a0044;
+        public static final int instr_towork2=0x7f0a0045;
+        public static final int instr_towork3=0x7f0a0046;
+        public static final int instr_towork4=0x7f0a0047;
+        public static final int main_instr_rl=0x7f0a003d;
         public static final int mainll=0x7f0a0000;
         public static final int monthyear=0x7f0a0002;
-        public static final int more_info=0x7f0a0041;
         public static final int navrl=0x7f0a0001;
         public static final int nextmonth=0x7f0a0004;
         public static final int prevmonth=0x7f0a0003;
+        public static final int sample_homescreen=0x7f0a003f;
+        public static final int showinstr_btn=0x7f0a0041;
         public static final int week1ll=0x7f0a000d;
         public static final int week2ll=0x7f0a0015;
         public static final int week3ll=0x7f0a001d;
@@ -140,44 +156,52 @@ public final class R {
         public static final int main_instructions=0x7f030002;
     }
     public static final class string {
-        public static final int about_settings=0x7f07000f;
-        public static final int app_name=0x7f070001;
-        public static final int app_settings=0x7f070007;
-        public static final int app_version=0x7f070002;
-        public static final int desc_next=0x7f070006;
-        public static final int desc_prev=0x7f070005;
-        public static final int feedback=0x7f07000a;
-        public static final int instructions_intro=0x7f070013;
-        public static final int instructions_text=0x7f070000;
-        public static final int more_info=0x7f070014;
-        public static final int options=0x7f070008;
-        public static final int summary_contactdev=0x7f07000c;
-        public static final int summary_donate=0x7f07000e;
-        public static final int summary_website=0x7f070011;
-        public static final int title_FDW=0x7f070009;
-        public static final int title_contactdev=0x7f07000b;
-        public static final int title_donate=0x7f07000d;
-        public static final int title_version=0x7f070012;
-        public static final int title_website=0x7f070010;
-        public static final int widget_3x2=0x7f070003;
-        public static final int widget_4x3=0x7f070004;
+        public static final int about_settings=0x7f07000e;
+        public static final int app_name=0x7f070000;
+        public static final int app_settings=0x7f070006;
+        public static final int app_version=0x7f070001;
+        public static final int desc_next=0x7f070005;
+        public static final int desc_prev=0x7f070004;
+        public static final int feedback=0x7f070009;
+        public static final int instr_sampleHS_description=0x7f070013;
+        public static final int instructions_intro=0x7f070012;
+        public static final int instructions_morehelp=0x7f07001b;
+        public static final int instructions_navigation=0x7f07001a;
+        public static final int instructions_shown=0x7f070015;
+        public static final int instructions_towork_1=0x7f070016;
+        public static final int instructions_towork_2=0x7f070017;
+        public static final int instructions_towork_3=0x7f070018;
+        public static final int instructions_towork_4=0x7f070019;
+        public static final int options=0x7f070007;
+        public static final int show_instructions=0x7f070014;
+        public static final int summary_contactdev=0x7f07000b;
+        public static final int summary_donate=0x7f07000d;
+        public static final int summary_website=0x7f070010;
+        public static final int title_FDW=0x7f070008;
+        public static final int title_contactdev=0x7f07000a;
+        public static final int title_donate=0x7f07000c;
+        public static final int title_version=0x7f070011;
+        public static final int title_website=0x7f07000f;
+        public static final int widget_3x2=0x7f070002;
+        public static final int widget_4x3=0x7f070003;
     }
     public static final class style {
         /**  3x2 
          */
-        public static final int Img_Arrow=0x7f090002;
+        public static final int Img_Arrow=0x7f090003;
         /**  4x3 
          */
-        public static final int Img_Arrow_4x3=0x7f090007;
+        public static final int Img_Arrow_4x3=0x7f090008;
         public static final int InstructionsTheme=0x7f090001;
-        public static final int LL_Week=0x7f090005;
+        public static final int Instructions_Font=0x7f090002;
+        public static final int LL_Week=0x7f090006;
         public static final int SettingsTheme=0x7f090000;
-        public static final int Text_Dates=0x7f090006;
-        public static final int Text_Dates_4x3=0x7f09000a;
-        public static final int Text_DaysOfWeek=0x7f090004;
-        public static final int Text_DaysOfWeek_4x3=0x7f090009;
-        public static final int Text_MonthYear=0x7f090003;
-        public static final int Text_MonthYear_4x3=0x7f090008;
+        public static final int Text_Dates=0x7f090007;
+        public static final int Text_Dates_4x3=0x7f09000b;
+        public static final int Text_DaysOfWeek=0x7f090005;
+        public static final int Text_DaysOfWeek_4x3=0x7f09000a;
+        public static final int Text_MonthYear=0x7f090004;
+        public static final int Text_MonthYear_4x3=0x7f090009;
     }
     public static final class xml {
         public static final int mcw_provider_3x2=0x7f040000;
--- src/com/josegd/monthcalwidget/MCWInstructions.java
+++ src/com/josegd/monthcalwidget/MCWInstructions.java
@@ -4,19 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MCWInstructions extends Activity {
 
-	private TextView tvMoreInfo;
+	private TextView tvMoreHelp;
+	private ScrollView svInstructions;
+	private Button btnShowInstructions;
+	private int buttonWidth;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_instructions);
-		tvMoreInfo = (TextView) findViewById(R.id.more_info);
-		tvMoreInfo.setMovementMethod(LinkMovementMethod.getInstance());
-		tvMoreInfo.setText(Html.fromHtml(getString(R.string.more_info)));
+		tvMoreHelp = (TextView) findViewById(R.id.instr_morehelp);
+		svInstructions = (ScrollView) findViewById(R.id.instr_howtouse_sv);
+		btnShowInstructions = (Button) findViewById(R.id.showinstr_btn);
+		tvMoreHelp.setMovementMethod(LinkMovementMethod.getInstance());
+		tvMoreHelp.setText(Html.fromHtml(getString(R.string.instructions_morehelp)));
+		buttonWidth = getResources().getDimensionPixelSize(R.dimen.instr_textsize_1);
+	}
+
+	public void showInstructions(View view) {
+		if (svInstructions.getVisibility() == View.VISIBLE) {
+			svInstructions.setVisibility(View.INVISIBLE);
+			btnShowInstructions.setText(R.string.show_instructions);
+			btnShowInstructions.setTextSize(TypedValue.COMPLEX_UNIT_PX, buttonWidth);
+		} else {
+			svInstructions.setVisibility(View.VISIBLE);
+			btnShowInstructions.setText(R.string.instructions_shown);
+			btnShowInstructions.setTextSize(TypedValue.COMPLEX_UNIT_PX, buttonWidth - 6);
+		}
 	}
 	
 }
